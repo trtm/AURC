cf67a1c8da591cc2c506c33c0d2b23c1a25c6bb6
Ðietrich ₸rautmann <trtm@users.noreply.github.com>
2020-05-17 14:20:11 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+pip install -r requirements.txt
+
 python3 src/download.py -w=0.1 --topic=abortion
 python3 src/download.py -w=0.1 --topic=cloning
 python3 src/download.py -w=0.1 --topic=death\ penalty
